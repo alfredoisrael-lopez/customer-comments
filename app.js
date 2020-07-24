57c306f9bc0be6585f7115346453e1251d3f00f5
alfredo-lopez <alfredo.lopez@mx1.ibm.com>
2020-07-24 16:43:22 -0500

    new change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Cloudpak for Apps");
+  res.send("Hello from Cloudpak for Apps new change");
 });
  
 module.exports.app = app;
